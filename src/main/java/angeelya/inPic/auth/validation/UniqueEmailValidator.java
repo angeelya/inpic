9efbe40923921806f3ab6371cd0fc0a7324e9906
Angelina <n.angelina169@gmail.com>
2024-04-28 10:16:00 +0300

    Authentication

--- src/main/java/angeelya/inPic/auth/validation/UniqueEmailValidator.java
+++ src/main/java/angeelya/inPic/auth/validation/UniqueEmailValidator.java
@@ -4,14 +4,15 @@ import angeelya.inPic.auth.repository.UserRepository;
 import angeelya.inPic.model.User;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import java.lang.annotation.Annotation;
 import java.util.Optional;
-
+@Component
+@RequiredArgsConstructor
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail,String> {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     @Override
     public void initialize(UniqueEmail constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
--- src/main/java/angeelya/inPic/auth/validation/UniqueLoginValidator.java
+++ src/main/java/angeelya/inPic/auth/validation/UniqueLoginValidator.java
@@ -4,13 +4,16 @@ import angeelya.inPic.auth.repository.UserRepository;
 import angeelya.inPic.model.User;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
-
+@Component
+@RequiredArgsConstructor
 public class UniqueLoginValidator implements ConstraintValidator<UniqueLogin,String> {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
     @Override
     public void initialize(UniqueLogin constraintAnnotation) {
     }
