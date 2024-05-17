9abf774e6dedbcc54c80cf19f5175ec97a37dcf2
Angelina <n.angelina169@gmail.com>
2024-05-17 22:44:44 +0300

    Module Exception

--- src/main/java/angeelya/inPic/exception_handling/GlobalControllerAdvice.java
+++ src/main/java/angeelya/inPic/exception_handling/GlobalControllerAdvice.java
@@ -9,16 +9,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
-    @ExceptionHandler({ValidationErrorsException.class,AuthException.class,PasswordUpdateException.class})
-    public ResponseEntity handlerExceptionBadRequest(Exception e) {
+    @ExceptionHandler({ValidationErrorsException.class, AuthException.class, PasswordUpdateException.class})
+    public ResponseEntity handlerBadRequestException(Exception e) {
         return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
     }
+
     @ExceptionHandler(NotFoundDatabaseException.class)
     public ResponseEntity handlerDatabaseNotFoundException(NotFoundDatabaseException e) {
         return new ResponseEntity(new MessageResponse(e.getMessage()), HttpStatus.NOT_FOUND);
     }
-    @ExceptionHandler({FileException.class, NoAddDatabaseException.class,DeleteDatabaseException.class})
-    public ResponseEntity handlerException(Exception e) {
+
+    @ExceptionHandler(ForbiddenRequestException.class)
+    public ResponseEntity handlerForbiddenException(ForbiddenRequestException e) {
+        return new ResponseEntity(new MessageResponse(e.getMessage()), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler({FileException.class, NoAddDatabaseException.class, DeleteDatabaseException.class})
+    public ResponseEntity handlerServerException(Exception e) {
         return new ResponseEntity(new MessageResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ src/main/java/angeelya/inPic/exception_handling/exception/ForbiddenRequestException.java
@@ -0,0 +1,7 @@
+package angeelya.inPic.exception_handling.exception;
+
+public class ForbiddenRequestException extends Exception{
+    public ForbiddenRequestException(String message) {
+        super(message);
+    }
+}
