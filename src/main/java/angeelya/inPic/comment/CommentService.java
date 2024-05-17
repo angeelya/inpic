c252b702ad7544d3d18b45f12f541e20c6f7adb7
Angelina <n.angelina169@gmail.com>
2024-05-17 18:56:59 +0300

    Module Comment

---
+++ src/main/java/angeelya/inPic/comment/CommentService.java
@@ -0,0 +1,10 @@
+package angeelya.inPic.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+}
