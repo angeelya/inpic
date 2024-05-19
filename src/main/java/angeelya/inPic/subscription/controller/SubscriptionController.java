ee6df846cf29b09c0f9ca4c5f607a935c453e5ec
Angelina <n.angelina169@gmail.com>
2024-05-20 01:40:27 +0300

    Module Subscription

---
+++ src/main/java/angeelya/inPic/subscription/controller/SubscriptionController.java
@@ -0,0 +1,69 @@
+package angeelya.inPic.subscription.controller;
+
+import angeelya.inPic.dto.request.*;
+import angeelya.inPic.dto.response.CountSubscriptionResponse;
+import angeelya.inPic.dto.response.MessageResponse;
+import angeelya.inPic.dto.response.UserSubscriberResponse;
+import angeelya.inPic.dto.response.UserSubscriptionResponse;
+import angeelya.inPic.exception_handling.exception.*;
+import angeelya.inPic.subscription.service.SubscriptionService;
+import angeelya.inPic.validation.service.ValidationErrorsService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/friend")
+@RequiredArgsConstructor
+public class SubscriptionController {
+    private final ValidationErrorsService validationErrorsService;
+    private final SubscriptionService subscriptionService;
+
+    @PostMapping("/add/friend")
+    public ResponseEntity<MessageResponse> addFriend(@RequestBody @Valid FriendAddRequest friendAddRequest, BindingResult bindingResult) throws ValidationErrorsException, NotFoundDatabaseException, NoAddDatabaseException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(new MessageResponse(subscriptionService.addFriend(friendAddRequest)));
+    }
+    @PostMapping("/counts")
+    public ResponseEntity<CountSubscriptionResponse> getCounts(@RequestBody @Valid UserInformationRequest userInformationRequest, BindingResult bindingResult) throws ValidationErrorsException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(subscriptionService.getCounts(userInformationRequest));
+    }
+    @PostMapping("/all/by/user/subscription")
+    public ResponseEntity<List<UserSubscriptionResponse>> getSubscriptions(@RequestBody @Valid UserInformationRequest userInformationRequest, BindingResult bindingResult) throws ValidationErrorsException, NotFoundDatabaseException, FileException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(subscriptionService.getUserSubscriptions(userInformationRequest));
+    }
+    @PostMapping("/all/by/user/subscriber")
+    public ResponseEntity<List<UserSubscriberResponse>> getSubscribers(@RequestBody @Valid UserInformationRequest userInformationRequest, BindingResult bindingResult) throws ValidationErrorsException, NotFoundDatabaseException, FileException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(subscriptionService.getUserSubscribers(userInformationRequest));
+    }
+    @PostMapping("/delete/subscription")
+    public ResponseEntity<MessageResponse> deleteSubscription(@RequestBody @Valid SubscriptionRequest subscriptionRequest, BindingResult bindingResult) throws ValidationErrorsException, DeleteDatabaseException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(new MessageResponse(subscriptionService.deleteSubscription(subscriptionRequest)));
+    }
+    @PostMapping("/delete/subscriber")
+    public ResponseEntity<MessageResponse> deleteSubscriber(@RequestBody @Valid SubscriberRequest subscriberRequest, BindingResult bindingResult) throws ValidationErrorsException, DeleteDatabaseException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(new MessageResponse(subscriptionService.deleteSubscriber(subscriberRequest)));
+    }
+    @PostMapping("/check/subscription")
+    public ResponseEntity<CheckFriendResponse> checkSubscription(@RequestBody @Valid SubscriptionRequest subscriptionRequest, BindingResult bindingResult) throws ValidationErrorsException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(subscriptionService.checkSubscription(subscriptionRequest));
+    }
+    @PostMapping("/check/subscriber")
+    public ResponseEntity<CheckFriendResponse> checkSubscriber(@RequestBody @Valid SubscriberRequest subscriberRequest, BindingResult bindingResult) throws ValidationErrorsException {
+        validationErrorsService.validation(bindingResult);
+        return ResponseEntity.ok(subscriptionService.checkSubscriber(subscriberRequest));
+    }
+}
---
+++ src/main/java/angeelya/inPic/subscription/service/SubscriptionService.java
@@ -0,0 +1,108 @@
+package angeelya.inPic.subscription.service;
+
+import angeelya.inPic.database.model.Friend;
+import angeelya.inPic.database.model.User;
+import angeelya.inPic.database.repository.FriendRepository;
+import angeelya.inPic.dto.request.*;
+import angeelya.inPic.dto.response.CountSubscriptionResponse;
+import angeelya.inPic.dto.response.UserSubscriberResponse;
+import angeelya.inPic.dto.response.UserSubscriptionResponse;
+import angeelya.inPic.exception_handling.exception.DeleteDatabaseException;
+import angeelya.inPic.exception_handling.exception.FileException;
+import angeelya.inPic.exception_handling.exception.NoAddDatabaseException;
+import angeelya.inPic.exception_handling.exception.NotFoundDatabaseException;
+import angeelya.inPic.file.service.ImageFileService;
+import angeelya.inPic.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SubscriptionService {
+    private final FriendRepository friendRepository;
+    private final UserService userService;
+    private final ImageFileService imageFileService;
+
+    public String addFriend(FriendAddRequest friendAddRequest) throws NotFoundDatabaseException, NoAddDatabaseException {
+        User user = userService.getUser(friendAddRequest.getUser_id()),
+                friend = userService.getUser(friendAddRequest.getFriend_id());
+        try {
+            friendRepository.save(Friend.builder().
+                    user(user)
+                    .subFriend(friend).build());
+        } catch (DataAccessException e) {
+            throw new NoAddDatabaseException("Failed to add friend");
+        }
+        return "Friend adding is successful";
+    }
+
+    public CountSubscriptionResponse getCounts(UserInformationRequest userInformationRequest) {
+        return CountSubscriptionResponse.builder().countSubscription(friendRepository.countByUser_Id(userInformationRequest.getUser_id()))
+                .countSubscribes(friendRepository.countBySubFriend_Id(userInformationRequest.getUser_id())).build();
+    }
+
+
+    public List<UserSubscriptionResponse> getUserSubscriptions(UserInformationRequest userInformationRequest) throws NotFoundDatabaseException, FileException {
+        List<Friend> friends = getFriend(userInformationRequest.getUser_id(), false);
+        List<UserSubscriptionResponse> userSubscriptionResponses = new ArrayList<>();
+        for (Friend friend : friends) {
+            userSubscriptionResponses.add(UserSubscriptionResponse.builder()
+                    .subscription_id(friend.getSubFriend().getId())
+                    .subName(friend.getSubFriend().getName())
+                    .subImage(imageFileService.getImage(friend.getSubFriend().getUserImage().getName())).build());
+        }
+        return userSubscriptionResponses;
+    }
+
+    public List<UserSubscriberResponse> getUserSubscribers(UserInformationRequest userInformationRequest) throws NotFoundDatabaseException, FileException {
+        List<Friend> friends = getFriend(userInformationRequest.getUser_id(), true);
+        List<UserSubscriberResponse> userSubscriberResponses = new ArrayList<>();
+        for (Friend friend : friends) {
+            userSubscriberResponses.add(UserSubscriberResponse.builder()
+                    .subscriber_id(friend.getUser().getId())
+                    .subName(friend.getUser().getName())
+                    .subImage(imageFileService.getImage(friend.getUser().getUserImage().getName())).build());
+        }
+        return userSubscriberResponses;
+    }
+
+    public String deleteSubscription(SubscriptionRequest subscriptionRequest) throws DeleteDatabaseException {
+        try {
+            friendRepository.deleteBySubFriend_IdAndUser_Id(subscriptionRequest.getSubscription_id(), subscriptionRequest.getUser_id());
+        } catch (EmptyResultDataAccessException e) {
+            throw new DeleteDatabaseException("Failed to delete subscription");
+        }
+        return "Subscription deleting is successful";
+    }
+
+    public String deleteSubscriber(SubscriberRequest subscriberRequest) throws DeleteDatabaseException {
+        try {
+            friendRepository.deleteByUser_IdAndSubFriend_Id(subscriberRequest.getSubscriber_id(), subscriberRequest.getUser_id());
+        } catch (EmptyResultDataAccessException e) {
+            throw new DeleteDatabaseException("Failed to delete subscriber");
+        }
+        return "Subscriber deleting is successful";
+    }
+
+    public CheckFriendResponse checkSubscription(SubscriptionRequest subscriptionRequest) {
+        Optional<Friend> friend = friendRepository.findBySubFriend_IdAndUser_Id(subscriptionRequest.getSubscription_id(), subscriptionRequest.getUser_id());
+        if (friend.isEmpty()) return CheckFriendResponse.builder().haveFriend(false).build();
+        return CheckFriendResponse.builder().haveFriend(true).build();
+    }
+    public CheckFriendResponse checkSubscriber(SubscriberRequest subscriberRequest) {
+        Optional<Friend> friend = friendRepository.findByUser_IdAndSubFriend_Id(subscriberRequest.getSubscriber_id(), subscriberRequest.getUser_id());
+        if (friend.isEmpty()) return CheckFriendResponse.builder().haveFriend(false).build();
+        return CheckFriendResponse.builder().haveFriend(true).build();
+    }
+    private List<Friend> getFriend(Long user_id, Boolean isSubscriber) throws NotFoundDatabaseException {
+        List<Friend> friends = isSubscriber ? friendRepository.findBySubFriend_Id(user_id) : friendRepository.findByUser_Id(user_id);
+        if (friends.isEmpty()) throw new NotFoundDatabaseException("No subscribers found");
+        return friends;
+    }
+}
