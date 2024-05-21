20145c15cd60cb33e454d575e537e0e8eb077318
Angelina <n.angelina169@gmail.com>
2024-05-21 12:51:24 +0300

    Module Admin

--- src/main/java/angeelya/inPic/administration/service/AdminService.java
+++ src/main/java/angeelya/inPic/administration/service/AdminService.java
@@ -1,5 +1,6 @@
 package angeelya.inPic.administration.service;
 
+import angeelya.inPic.database.model.AdminNotification;
 import angeelya.inPic.database.model.DeletedImage;
 import angeelya.inPic.database.model.Image;
 import angeelya.inPic.database.model.User;
@@ -12,12 +13,13 @@ import angeelya.inPic.exception_handling.exception.FileException;
 import angeelya.inPic.exception_handling.exception.NoAddDatabaseException;
 import angeelya.inPic.exception_handling.exception.NotFoundDatabaseException;
 import angeelya.inPic.file.service.ImageFileService;
-import angeelya.inPic.image.service.ImageService;
+import angeelya.inPic.image.service.ImageGetService;
 import angeelya.inPic.notification.service.AdminNotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,40 +30,46 @@ import java.util.Optional;
 public class AdminService {
     private final ImageRepository imageRepository;
     private final ImageFileService imageFileService;
-    private final ImageService imageService;
+    private final ImageGetService imageGetService;
     private final DeletedImageRepository deletedImageRepository;
     private final AdminNotificationService adminNotificationService;
+    private static final String MS_NOT_FOUND_POSTS = "No image posts found", MS_FAILED_DELETE = "Failed to delete image",
+            MS_SUCCESS_DELETE = "Image deleting is successful", MS_FAILED_ADD_DELETED_IMAGE = "Failed to add deleted image";
 
     public List<ImageResponse> getAllImagePost() throws NotFoundDatabaseException, FileException {
         List<Image> images = imageRepository.findAll();
-        if (images.isEmpty()) throw new NotFoundDatabaseException("No image posts found");
+        if (images.isEmpty()) throw new NotFoundDatabaseException(MS_NOT_FOUND_POSTS);
         List<ImageResponse> imageResponses = new ArrayList<>();
         for (Image image : images) {
             imageResponses.add(ImageResponse.builder()
                     .image_id(image.getId())
+                    .imgName(image.getImgName())
                     .name(image.getName())
                     .image(imageFileService.getImage(image.getImgName())).build());
         }
         return imageResponses;
     }
 
-    public String deleteImagePost(ImageRequest imageRequest) throws DeleteDatabaseException, NotFoundDatabaseException, FileException, NoAddDatabaseException {
-        Image image = imageService.getImage(imageRequest.getImage_id());
+    @Transactional
+    public String deleteImagePost(ImageRequest imageRequest) throws DeleteDatabaseException, NotFoundDatabaseException, NoAddDatabaseException {
+        Image image = imageGetService.getImage(imageRequest.getImage_id());
         User user = image.getUser();
         delete(image);
         Optional<Image> imageOptional = imageRepository.findById(imageRequest.getImage_id());
-        if (imageOptional.isPresent()) throw new DeleteDatabaseException("Failed to delete image");
-        DeletedImage deletedImage=saveDeletedImage(DeletedImage.builder().imgName(image.getImgName())
-                .path(image.getPath()).build());
-        adminNotificationService.addNotification(deletedImage, user);
-        return "Image deleting is successful";
+        if (imageOptional.isPresent()) throw new DeleteDatabaseException(MS_FAILED_DELETE);
+        DeletedImage deletedImage = DeletedImage.builder().imgName(image.getImgName())
+                .path(image.getPath()).build();
+        AdminNotification adminNotification = adminNotificationService.addNotification(deletedImage, user);
+        deletedImage.setAdminNotification(adminNotification);
+        saveDeletedImage(deletedImage);
+        return MS_SUCCESS_DELETE;
     }
 
-    private DeletedImage saveDeletedImage(DeletedImage deletedImage) throws NoAddDatabaseException {
+    private void saveDeletedImage(DeletedImage deletedImage) throws NoAddDatabaseException {
         try {
-            return deletedImageRepository.save(deletedImage);
+             deletedImageRepository.save(deletedImage);
         } catch (DataAccessException e) {
-            throw new NoAddDatabaseException("Failed to add deleted image");
+            throw new NoAddDatabaseException(MS_FAILED_ADD_DELETED_IMAGE);
         }
     }
 
@@ -69,7 +77,7 @@ public class AdminService {
         try {
             imageRepository.delete(image);
         } catch (EmptyResultDataAccessException e) {
-            throw new DeleteDatabaseException("Failed to delete image");
+            throw new DeleteDatabaseException(MS_FAILED_DELETE);
         }
     }
 }
