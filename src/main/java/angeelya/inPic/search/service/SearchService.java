143d509cb7a35b86e23bf18c96de540e98eed37b
Angelina <n.angelina169@gmail.com>
2024-05-21 13:02:15 +0300

    Module Search

--- src/main/java/angeelya/inPic/search/service/SearchService.java
+++ src/main/java/angeelya/inPic/search/service/SearchService.java
@@ -12,7 +12,7 @@ import angeelya.inPic.file.service.ImageFileService;
 import angeelya.inPic.dto.request.UserInformationRequest;
 import angeelya.inPic.dto.request.SearchImageRequest;
 import angeelya.inPic.dto.response.ImageResponse;
-import angeelya.inPic.user.service.UserService;
+import angeelya.inPic.user.service.UserGetService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,33 +30,35 @@ public class SearchService {
     private final ImageRepository imageRepository;
     private final ImageFileService imageFileService;
     private final SearchRepository searchRepository;
-    private final UserService userService;
+    private final UserGetService userGetService;
+    private static final String MS_NOT_FOUND_LIST ="No images found",MS_NOT_EXIST_FILE="Such image does not exist",
+    MS_NOT_FOUND_HISTORY ="Such search history does not exist",MS_FAILED_SAVE="Failed to save search";
 
     public List<ImageResponse> searchImage(SearchImageRequest searchImageRequest) throws FileException, NotFoundDatabaseException, NoAddDatabaseException {
         String key = searchImageRequest.getKey();
         saveRequestHistory(key, searchImageRequest.getUser_id());
-        List<Image> images = imageRepository.findByNameIsLikeIgnoreCaseOrCategory_CategoryIsLikeIgnoreCaseOrDescriptionIsLikeIgnoreCase(key, key, key);
-        if (images.isEmpty()) throw new NotFoundDatabaseException("No images found");
+        List<Image> images = imageRepository.findByNameIsLikeIgnoreCaseOrCategory_CategoryIsLikeIgnoreCaseOrDescriptionIsLikeIgnoreCase(key+"%", key+"%", key+"%");
+        if (images.isEmpty()) throw new NotFoundDatabaseException(MS_NOT_FOUND_LIST);
         List<ImageResponse> imageResponses = images.stream().map(image -> {
             try {
-                return new ImageResponse(image.getId(), imageFileService.getImage(image.getImgName()), image.getName());
+                return new ImageResponse(image.getId(),image.getImgName(), imageFileService.getImage(image.getImgName()), image.getName());
             } catch (FileException e) {
                 logger.error(e.getMessage());
                 return null;
             }
         }).filter(Objects::nonNull).collect(Collectors.toList());
-        if (imageResponses.isEmpty()) throw new FileException("Such image does not exist");
+        if (imageResponses.isEmpty()) throw new FileException(MS_NOT_EXIST_FILE);
         return imageResponses;
     }
 
     private void saveRequestHistory(String text, Long user_id) throws NotFoundDatabaseException, NoAddDatabaseException {
-        User user = userService.getUser(user_id);
+        User user = userGetService.getUser(user_id);
         saveSearch(Search.builder().text(text).user(user).build());
     }
 
     public List<Search> getSearchesHistory(UserInformationRequest userInformationRequest) throws NotFoundDatabaseException {
         List<Search> searches = searchRepository.findByUser_Id(userInformationRequest.getUser_id());
-        if (searches.isEmpty()) throw new NotFoundDatabaseException("Such search history does not exist");
+        if (searches.isEmpty()) throw new NotFoundDatabaseException(MS_NOT_FOUND_HISTORY);
         return searches;
     }
 
@@ -64,7 +66,7 @@ public class SearchService {
         try {
             searchRepository.save(search);
         } catch (DataAccessException e) {
-            throw new NoAddDatabaseException("Failed to save search");
+            throw new NoAddDatabaseException(MS_FAILED_SAVE);
         }
     }
 }
