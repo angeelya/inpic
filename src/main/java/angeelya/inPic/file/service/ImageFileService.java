6b1d99a6fdbf0eceae615489f5724d4fa0eab6cf
Angelina <n.angelina169@gmail.com>
2024-05-21 12:58:37 +0300

    Module File

--- src/main/java/angeelya/inPic/file/service/ImageFileService.java
+++ src/main/java/angeelya/inPic/file/service/ImageFileService.java
@@ -3,30 +3,27 @@ package angeelya.inPic.file.service;
 import angeelya.inPic.exception_handling.exception.FileException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
 @Service
 public class ImageFileService {
-    private final Path path = Paths.get("image");
-    private File directory;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final String MS_FAILED_SAVE = "Failed to save image file", MS_FAILED_GET = "Failed to get image file",
-            MS_FAILED_DELETE="Failed to delete image file";
+    private final Path path = Paths.get("files");
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final String MS_FAILED_SAVE = "Failed to save image file", MS_FAILED_GET = "Failed to get image file",
+            MS_FAILED_DELETE="Failed to delete image file", MS_CREATE="Created ";
     ImageFileService() {
-        directory = new File(path.toString());
+        File directory = new File(path.toString());
         if (!directory.exists()) {
             directory.mkdir();
-            logger.info("Created" + directory);
+            logger.info(MS_CREATE + directory);
         }
     }
 
@@ -39,13 +36,11 @@ public class ImageFileService {
         }
     }
 
-    public Resource getImage(String imgName) throws FileException {
+    public String getImage(String imgName) throws FileException {
         try {
-            Resource resource = new UrlResource(path.resolve(imgName).toUri());
-            if (resource.exists() || resource.isFile() || resource.isReadable())
-                return resource;
-            else throw new FileException("Couldn't load the file.");
-        } catch (MalformedURLException | FileException e) {
+            byte[] imageByte = Files.readAllBytes(new File(path+"/"+imgName).toPath());
+                return Base64.getEncoder().encodeToString(imageByte);
+        } catch (IOException e) {
             logger.error(e.getMessage());
             throw new FileException(MS_FAILED_GET);
         }
