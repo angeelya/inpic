cad3eb232da3978743571c4178cbc1ebe0b7134f
Angelina <n.angelina169@gmail.com>
2024-05-21 12:56:39 +0300

    Module Exception

--- src/main/java/angeelya/inPic/exception_handling/GlobalControllerAdvice.java
+++ src/main/java/angeelya/inPic/exception_handling/GlobalControllerAdvice.java
@@ -12,22 +12,22 @@ import org.springframework.web.multipart.MultipartException;
 @ControllerAdvice
 public class GlobalControllerAdvice {
     @ExceptionHandler({ValidationErrorsException.class, AuthException.class, PasswordUpdateException.class,ExistException.class})
-    public ResponseEntity handlerBadRequestException(Exception e) {
+    public ResponseEntity<MessageResponse> handlerBadRequestException(Exception e) {
         return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
     }
 
     @ExceptionHandler({NotFoundDatabaseException.class, EntityNotFoundException.class})
-    public ResponseEntity handlerDatabaseNotFoundException(NotFoundDatabaseException e) {
-        return new ResponseEntity(new MessageResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<MessageResponse> handlerDatabaseNotFoundException(NotFoundDatabaseException e) {
+        return new ResponseEntity<>(new MessageResponse(e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ForbiddenRequestException.class)
-    public ResponseEntity handlerForbiddenException(ForbiddenRequestException e) {
-        return new ResponseEntity(new MessageResponse(e.getMessage()), HttpStatus.FORBIDDEN);
+    public ResponseEntity<MessageResponse> handlerForbiddenException(ForbiddenRequestException e) {
+        return new ResponseEntity<>(new MessageResponse(e.getMessage()), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler({FileException.class, NoAddDatabaseException.class, DeleteDatabaseException.class, MultipartException.class})
-    public ResponseEntity handlerServerException(Exception e) {
-        return new ResponseEntity(new MessageResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<MessageResponse> handlerServerException(Exception e) {
+        return new ResponseEntity<>(new MessageResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
