2eeb499a5f48dabc7e736286f2fe01a7eb3cfb28
Angelina <n.angelina169@gmail.com>
2024-05-23 18:21:13 +0300

    Module Validation (Test)

---
+++ src/test/java/angeelya/inPic/validation/service/ValidationErrorsServiceTest.java
@@ -0,0 +1,40 @@
+package angeelya.inPic.validation.service;
+
+import angeelya.inPic.exception_handling.exception.ValidationErrorsException;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.MapBindingResult;
+import org.springframework.validation.ObjectError;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class ValidationErrorsServiceTest {
+    @InjectMocks
+    ValidationErrorsService validation;
+    @SneakyThrows
+    @Test
+    void testValidation() {
+        BindingResult bindingResult = mock(BindingResult.class);
+        when(bindingResult.hasErrors()).thenReturn(true);
+        assertThrows(ValidationErrorsException.class, () -> validation.validation(bindingResult));
+        BindingResult bindingResultTrue = mock(BindingResult.class);
+        when(bindingResult.hasErrors()).thenReturn(false);
+        validation.validation(bindingResultTrue);
+    }
+}
